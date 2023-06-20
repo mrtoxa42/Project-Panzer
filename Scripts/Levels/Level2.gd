e6450aa55f55ae991cb017f3691d6437b237d440
Emre <emreee.dindar@gmail.com>
2023-06-20 15:44:36 +0300

    tank ucu
    tankın ucu güncellendi fena olmadı ama en fazla bu kadar oluyor

--- Scripts/Levels/Level2.gd
+++ Scripts/Levels/Level2.gd
@@ -8,18 +8,18 @@ var staticenemy = preload("res://Scenes/Enemies/StaticEnemy.tscn")
 var rng = RandomNumberGenerator.new()
 var rng2 = RandomNumberGenerator.new()
 func _ready():
-	if GameManager.game_data.Language == "Turkish":
-		$Cinematic2/OrgLabel.show()
-		$Cinematic2/OrgLabel2.hide()
-		$Cinematic2/OrgLabel3.hide()
-	elif GameManager.game_data.Language == "English":
-		$Cinematic2/OrgLabel.hide()
-		$Cinematic2/OrgLabel2.show()
-		$Cinematic2/OrgLabel3.hide()
-	elif GameManager.game_data.Language == "Russian":
-		$Cinematic2/OrgLabel.hide()
-		$Cinematic2/OrgLabel2.hide()
-		$Cinematic2/OrgLabel3.show()
+#	if GameManager.game_data.Language == "Turkish":
+##		$Cinematic2/OrgLabel.show()
+##		$Cinematic2/OrgLabel2.hide()
+##		$Cinematic2/OrgLabel3.hide()
+#	elif GameManager.game_data.Language == "English":
+#		$Cinematic2/OrgLabel.hide()
+#		$Cinematic2/OrgLabel2.show()
+#		$Cinematic2/OrgLabel3.hide()
+#	elif GameManager.game_data.Language == "Russian":
+#		$Cinematic2/OrgLabel.hide()
+#		$Cinematic2/OrgLabel2.hide()
+#		$Cinematic2/OrgLabel3.show()
 	GameManager.level2station = $StationArea
 	$AnimationPlayer.play("Ani1")
 	GameManager.playedlevel = self
