45d11dbf32058c352bc9415bf29c62235e6a70c8
mrtoxa42 <betebet16@hotmail.com>
2023-06-27 03:41:53 +0300

    a
    a

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -62,3 +62,33 @@ func _on_AnimationPlayer3_animation_finished(anim_name):
 
 func explo():
 	$AnimationPlayer.play("ExplosionAni")
+
+
+func _on_FeedBackButton_pressed():
+	$FeedBack/LineEdit.show()
+	$FeedBack/GoMail.show()
+	$FeedBack/BackBlackButton.show()
+	$FeedBack/BlackSprite.show()
+	get_tree().paused = true
+
+
+func _on_GoMail_pressed():
+	var body = $FeedBack/LineEdit.text
+	OS.shell_open("mailto:studyo42@outlook.com.tr?subject=Project Panzer Geri Bildirimi&body=" + body)
+
+
+func _on_BackBlackButton_pressed():
+	$FeedBack/LineEdit.hide()
+	$FeedBack/GoMail.hide()
+	$FeedBack/BackBlackButton.hide()
+	$FeedBack/BlackSprite.hide()
+	var timer = Timer.new()
+	get_tree().get_root().add_child(timer)
+	timer.pause_mode = PAUSE_MODE_PROCESS
+	print(timer)
+	timer.connect("timeout",self,"on_timeout")
+	timer.wait_time = 0.2
+	timer.one_shot = true
+	timer.start()
+func on_timeout():
+	get_tree().paused = false
