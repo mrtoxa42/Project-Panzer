10e1ae7d80d8ac19c4c6debf14624e9e5017f535
mrtoxa42 <betebet16@hotmail.com>
2023-08-13 22:06:36 +0300

    Genel Güncellemeler.
    Baya şey yaptık bro ekip

--- Scripts/Levels/BossScenes/BossFight.gd
+++ Scripts/Levels/BossScenes/BossFight.gd
@@ -5,32 +5,32 @@ var touch = false
 
 
 func _process(delta):
-	$HitlerHp.value = GameManager.bosshitler.hp
-	$PlayerHp.value = GameManager.bossplayer.hp
+	$Invisible/HitlerHp.value = GameManager.bosshitler.hp
+	$Invisible/PlayerHp.value = GameManager.bossplayer.hp
 	
 
 
 func _on_AttackButton_pressed():
 	if touch == false:
 		touch = true
-		$HitlerLabel.text = "* Bende bunu istiyordum"
-		$PlayerLabel.text = "* Savun kendini hain kostok"
-		$ExploTimer.start()
+		$Invisible/HitlerLabel.text = "* Bende bunu istiyordum"
+		$Invisible/PlayerLabel.text = "* Savun kendini hain kostok"
+		$Invisible/ExploTimer.start()
 	
 
 
 func _on_SpeakButton_pressed():
 	if touch == false:
 		touch = true
-		$HitlerLabel.text = "*Artık konuşulacak bir şey" + "\n" + "kalmadı kiril"
-		$PlayerLabel.text = "* Konuşarak çözemezmiyiz?"
+		$Invisible/HitlerLabel.text = "*Artık konuşulacak bir şey" + "\n" + "kalmadı kiril"
+		$Invisible/PlayerLabel.text = "* Konuşarak çözemezmiyiz?"
 		$ExploTimer.start()
 
 func _on_MercyButton_pressed():
 	if touch == false:
 		touch = true
-		$HitlerLabel.text = "*Bu bir oyun değil gel " +"\n " + "ve benimle savaş"
-		$PlayerLabel.text = "*Canını bağışlıyorum yıkıl karşımdan!"
+		$Invisible/HitlerLabel.text = "*Bu bir oyun değil gel " +"\n " + "ve benimle savaş"
+		$Invisible/PlayerLabel.text = "*Canını bağışlıyorum yıkıl karşımdan!"
 		$ExploTimer.start()
 
 
@@ -43,21 +43,21 @@ func _on_ExploTimer_timeout():
 	explo()
 	GameManager.bossplayer.play_fight()
 	GameManager.bosshitler.play_fight()
-	$HitlerHp.show()
-	$PlayerHp.show()
+	$Invisible/HitlerHp.show()
+	$Invisible/PlayerHp.show()
 
 
 func _on_DontPressButton_pressed():
 	if touch == false:
 		touch = true
-		$HitlerLabel.text = "*Ge.. gerçekten mi?"
-		$PlayerLabel.text = "* Aslında ben nazileri çok severim"
+		$Invisible/HitlerLabel.text = "*Ge.. gerçekten mi?"
+		$Invisible/PlayerLabel.text = "* Aslında ben nazileri çok severim"
 		$ExploTimer.start()
 
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Talking":
-		$AnimationPlayer.play("ExplosionAni")
+		$AnimationPlayer.play("AniPull")
 	
 	
